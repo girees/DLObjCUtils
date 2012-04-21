5d66c5e0ea8c3810ef521d439b6f0c21b01c05c6
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2012-04-21 09:44:13 +0100

    Xcode project for compiling library for use as a subproject (via a git submodule) in other projects.

---
+++ Project/DLObjCUtils/DLObjCUtils.h
@@ -0,0 +1,36 @@
+//
+//  DLObjCUtils.h
+//  DLObjCUtils
+//
+//  Created by James Montgomerie on 21/04/2012.
+//  Copyright (c) 2012 Things Made Out Of Other Things. All rights reserved.
+//
+
+#import <DLObjCUtils/FPTuple.h>
+#import <DLObjCUtils/NSArray+FPAdditions.h>
+#import <DLObjCUtils/NSData+Base64.h>
+#import <DLObjCUtils/NSData+CocoaDevUsersAdditions.h>
+#import <DLObjCUtils/NSDate+Additions.h>
+#import <DLObjCUtils/NSObject+KVCExtensions.h>
+#import <DLObjCUtils/NSObject+ObserveActions.h>
+#import <DLObjCUtils/NSString+PercentEscape.h>
+#import <DLObjCUtils/NSString+StringAdditions.h>
+#import <DLObjCUtils/NSURL+ParameterParsing.h>
+#import <DLObjCUtils/PathUtils.h>
+#import <DLObjCUtils/DLFolding.h>
+#import <DLObjCUtils/SynthesizeSingleton.h>
+#import <DLObjCUtils/InnerShadowDrawing.h>
+#import <DLObjCUtils/UIColor+HelperAdditions.h>
+#import <DLObjCUtils/UIControl+DLBlockAdditions.h>
+#import <DLObjCUtils/UIImage+Additions.h>
+#import <DLObjCUtils/UIImage+Resize.h>
+#import <DLObjCUtils/UIImage+RoundedCorner.h>
+#import <DLObjCUtils/UIImage+Alpha.h>
+#import <DLObjCUtils/UIKit+DrawingHelpers.h>
+#import <DLObjCUtils/UITabBarController+HideableTabBar.h>
+#import <DLObjCUtils/UIView+Additions.h>
+#import <DLObjCUtils/UIView+Sliding.h>
+#import <DLObjCUtils/PopoverManager.h>
+#import <DLObjCUtils/UIPlaceHolderTextView.h>
+#import <DLObjCUtils/TimeHelpers.h>
+#import <DLObjCUtils/Util.h>
